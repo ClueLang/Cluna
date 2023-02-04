c0f8f32b81b7792e0ae170d2b70c453721ab89d8
MarkosTh09 <markos.theocharis2009@gmail.com>
2023-02-04 14:33:14 +0400

    chore: convert lua5.1 tests to utf-8

--- lua5.1-tests/all.lua
+++ lua5.1-tests/all.lua
@@ -1,4 +1,4 @@
-#!../lua
+--#!../lua
 
 math.randomseed(0)
 
@@ -16,41 +16,40 @@ print("current path:\n  " .. string.gsub(package.path, ";", "\n  "))
 
 
 local msgs = {}
-function Message (m)
+function Message(m)
   print(m)
-  msgs[#msgs+1] = string.sub(m, 3, -3)
+  msgs[#msgs + 1] = string.sub(m, 3, -3)
 end
-
 
 local c = os.clock()
 
-assert(os.setlocale"C")
+assert(os.setlocale "C")
 
-local T,print,gcinfo,format,write,assert,type =
-      T,print,gcinfo,string.format,io.write,assert,type
+local T, print, gcinfo, format, write, assert, type =
+T, print, gcinfo, string.format, io.write, assert, type
 
-local function formatmem (m)
+local function formatmem(m)
   if m < 1024 then return m
   else
-    m = m/1024 - m/1024%1
-    if m < 1024 then return m.."K"
+    m = m / 1024 - m / 1024 % 1
+    if m < 1024 then return m .. "K"
     else
-      m = m/1024 - m/1024%1
-      return m.."M"
+      m = m / 1024 - m / 1024 % 1
+      return m .. "M"
     end
   end
 end
 
-local showmem = function ()
+local showmem = function()
   if not T then
     print(format("    ---- total memory: %s ----\n", formatmem(gcinfo())))
   else
     T.checkmemory()
-    local a,b,c = T.totalmem()
-    local d,e = gcinfo()
+    local a, b, c = T.totalmem()
+    local d, e = gcinfo()
     print(format(
-  "\n    ---- total memory: %s (%dK), max use: %s,  blocks: %d\n",
-                        formatmem(a),  d,      formatmem(c),           b))
+      "\n    ---- total memory: %s (%dK), max use: %s,  blocks: %d\n",
+      formatmem(a), d, formatmem(c), b))
   end
 end
 
@@ -58,7 +57,7 @@ end
 --
 -- redefine dofile to run files through dump/undump
 --
-dofile = function (n)
+dofile = function(n)
   showmem()
   local f = assert(loadfile(n))
   local b = string.dump(f)
@@ -71,8 +70,8 @@ dofile('main.lua')
 do
   local u = newproxy(true)
   local newproxy, stderr = newproxy, io.stderr
-  getmetatable(u).__gc = function (o)
-    stderr:write'.'
+  getmetatable(u).__gc = function(o)
+    stderr:write '.'
     newproxy(o)
   end
 end
@@ -101,12 +100,13 @@ dofile('closure.lua')
 dofile('errors.lua')
 dofile('math.lua')
 dofile('sort.lua')
-assert(dofile('verybig.lua') == 10); collectgarbage()
+assert(dofile('verybig.lua') == 10);
+collectgarbage()
 dofile('files.lua')
 
 if #msgs > 0 then
   print("\ntests not performed:")
-  for i=1,#msgs do
+  for i = 1, #msgs do
     print(msgs[i])
   end
   print()
@@ -115,12 +115,12 @@ end
 print("final OK !!!")
 print('cleaning all!!!!')
 
-debug.sethook(function (a) assert(type(a) == 'string') end, "cr")
+debug.sethook(function(a) assert(type(a) == 'string') end, "cr")
 
 local _G, collectgarbage, showmem, print, format, clock =
-      _G, collectgarbage, showmem, print, format, os.clock
+_G, collectgarbage, showmem, print, format, os.clock
 
-local a={}
+local a = {}
 for n in pairs(_G) do a[n] = 1 end
 a.tostring = nil
 a.___Glob = nil
@@ -132,6 +132,7 @@ collectgarbage()
 collectgarbage()
 collectgarbage()
 collectgarbage()
-collectgarbage();showmem()
+collectgarbage();
+showmem()
 
-print(format("\n\ntotal time: %.2f\n", clock()-c))
+print(format("\n\ntotal time: %.2f\n", clock() - c))
--- lua5.1-tests/files.lua
+++ lua5.1-tests/files.lua
@@ -1,4 +1,3 @@
-
 print('testing i/o')
 
 assert(io.input(io.stdin) == io.stdin)
@@ -7,13 +6,14 @@ assert(io.output(io.stdout) == io.stdout)
 
 assert(type(io.input()) == "userdata" and io.type(io.output()) == "file")
 assert(io.type(8) == nil)
-local a = {}; setmetatable(a, {})
+local a = {};
+setmetatable(a, {})
 assert(io.type(a) == nil)
 
-local a,b,c = io.open('xuxu_nao_existe')
+local a, b, c = io.open('xuxu_nao_existe')
 assert(not a and type(b) == "string" and type(c) == "number")
 
-a,b,c = io.open('/a/b/c/d', 'w')
+a, b, c = io.open('/a/b/c/d', 'w')
 assert(not a and type(b) == "string" and type(c) == "number")
 
 local file = os.tmpname()
@@ -21,7 +21,8 @@ local otherfile = os.tmpname()
 
 assert(os.setlocale('C', 'all'))
 
-io.input(io.stdin); io.output(io.stdout);
+io.input(io.stdin);
+io.output(io.stdout);
 
 os.remove(file)
 assert(loadfile(file) == nil)
@@ -32,23 +33,23 @@ assert(io.output() ~= io.stdout)
 assert(io.output():seek() == 0)
 assert(io.write("alo alo"))
 assert(io.output():seek() == string.len("alo alo"))
-assert(io.output():seek("cur", -3) == string.len("alo alo")-3)
+assert(io.output():seek("cur", -3) == string.len("alo alo") - 3)
 assert(io.write("joao"))
 assert(io.output():seek("end") == string.len("alo joao"))
 
 assert(io.output():seek("set") == 0)
 
-assert(io.write('"álo"', "{a}\n", "second line\n", "third line \n"))
-assert(io.write('çfourth_line'))
+assert(io.write('"ï¿½lo"', "{a}\n", "second line\n", "third line \n"))
+assert(io.write('ï¿½fourth_line'))
 io.output(io.stdout)
-collectgarbage()  -- file should be closed by GC
+collectgarbage() -- file should be closed by GC
 assert(io.input() == io.stdin and rawequal(io.output(), io.stdout))
 print('+')
 
 -- test GC for files
 collectgarbage()
-for i=1,120 do
-  for i=1,5 do
+for i = 1, 120 do
+  for i = 1, 5 do
     io.input(file)
     assert(io.open(file, 'r'))
     io.lines(file)
@@ -67,9 +68,9 @@ io.close()
 assert(os.rename(otherfile, file))
 io.output(otherfile)
 local f = io.lines(file)
-while f() do end;
-assert(not pcall(f))  -- read lines after EOF
-assert(not pcall(f))  -- read lines after EOF
+while f() do end
+assert(not pcall(f)) -- read lines after EOF
+assert(not pcall(f)) -- read lines after EOF
 -- copy from file to otherfile
 for l in io.lines(file) do io.write(l, "\n") end
 io.close()
@@ -79,8 +80,9 @@ assert(io.type(f) == "file")
 io.output(file)
 assert(io.output():read() == nil)
 for l in f:lines() do io.write(l, "\n") end
-assert(f:close()); io.close()
-assert(not pcall(io.close, f))   -- error trying to close again
+assert(f:close());
+io.close()
+assert(not pcall(io.close, f)) -- error trying to close again
 assert(tostring(f) == "file (closed)")
 assert(io.type(f) == "closed file")
 io.input(file)
@@ -89,34 +91,34 @@ for l in io.lines() do assert(l == f()) end
 assert(os.remove(otherfile))
 
 io.input(file)
-do  -- test error returns
-  local a,b,c = io.input():write("xuxu")
+do -- test error returns
+  local a, b, c = io.input():write("xuxu")
   assert(not a and type(b) == "string" and type(c) == "number")
 end
-assert(io.read(0) == "")   -- not eof
-assert(io.read(5, '*l') == '"álo"')
+assert(io.read(0) == "") -- not eof
+assert(io.read(5, '*l') == '"ï¿½lo"')
 assert(io.read(0) == "")
 assert(io.read() == "second line")
 local x = io.input():seek()
 assert(io.read() == "third line ")
 assert(io.input():seek("set", x))
 assert(io.read('*l') == "third line ")
-assert(io.read(1) == "ç")
-assert(io.read(string.len"fourth_line") == "fourth_line")
-assert(io.input():seek("cur", -string.len"fourth_line"))
+assert(io.read(1) == "ï¿½")
+assert(io.read(string.len "fourth_line") == "fourth_line")
+assert(io.input():seek("cur", -string.len "fourth_line"))
 assert(io.read() == "fourth_line")
-assert(io.read() == "")  -- empty line
+assert(io.read() == "") -- empty line
 assert(io.read('*n') == 3450)
 assert(io.read(1) == '\n')
-assert(io.read(0) == nil)  -- end of file
-assert(io.read(1) == nil)  -- end of file
-assert(({io.read(1)})[2] == nil)
-assert(io.read() == nil)  -- end of file
-assert(({io.read()})[2] == nil)
-assert(io.read('*n') == nil)  -- end of file
-assert(({io.read('*n')})[2] == nil)
-assert(io.read('*a') == '')  -- end of file (OK for `*a')
-assert(io.read('*a') == '')  -- end of file (OK for `*a')
+assert(io.read(0) == nil) -- end of file
+assert(io.read(1) == nil) -- end of file
+assert(({ io.read(1) })[2] == nil)
+assert(io.read() == nil) -- end of file
+assert(({ io.read() })[2] == nil)
+assert(io.read('*n') == nil) -- end of file
+assert(({ io.read('*n') })[2] == nil)
+assert(io.read('*a') == '') -- end of file (OK for `*a')
+assert(io.read('*a') == '') -- end of file (OK for `*a')
 collectgarbage()
 print('+')
 io.close(io.input())
@@ -125,8 +127,8 @@ assert(not pcall(io.read))
 assert(os.remove(file))
 
 local t = '0123456789'
-for i=1,12 do t = t..t; end
-assert(string.len(t) == 10*2^12)
+for i = 1, 12 do t = t .. t; end
+assert(string.len(t) == 10 * 2 ^ 12)
 
 io.output(file)
 io.write("alo\n")
@@ -138,7 +140,8 @@ collectgarbage()
 
 assert(io.write(' ' .. t .. ' '))
 assert(io.write(';', 'end of file\n'))
-f:flush(); io.flush()
+f:flush();
+io.flush()
 f:close()
 print('+')
 
@@ -180,14 +183,14 @@ assert(filehandle ~= otherfilehandle)
 assert(type(filehandle) == "userdata")
 assert(filehandle:read('*l') == "qualquer coisa")
 io.input(otherfilehandle)
-assert(io.read(string.len"outra coisa") == "outra coisa")
+assert(io.read(string.len "outra coisa") == "outra coisa")
 assert(filehandle:read('*l') == "mais qualquer coisa")
 filehandle:close();
 assert(type(filehandle) == "userdata")
 io.input(otherfilehandle)
 assert(io.read(4) == "\0\1\3\0")
 assert(io.read(3) == "\0\0\0")
-assert(io.read(0) == "")        -- 255 is not eof
+assert(io.read(0) == "") -- 255 is not eof
 assert(io.read(1) == "\255")
 assert(io.read('*a') == "\0")
 assert(not io.read(0))
@@ -198,7 +201,7 @@ assert(os.remove(otherfile))
 collectgarbage()
 
 io.output(file)
-io.write[[
+io.write [[
  123.4	-56e-2  not a number
 second line
 third line
@@ -207,12 +210,12 @@ and the rest of the file
 ]]
 io.close()
 io.input(file)
-local _,a,b,c,d,e,h,__ = io.read(1, '*n', '*n', '*l', '*l', '*l', '*a', 10)
+local _, a, b, c, d, e, h, __ = io.read(1, '*n', '*n', '*l', '*l', '*l', '*a', 10)
 assert(io.close(io.input()))
 assert(_ == ' ' and __ == nil)
-assert(type(a) == 'number' and a==123.4 and b==-56e-2)
-assert(d=='second line' and e=='third line')
-assert(h==[[
+assert(type(a) == 'number' and a == 123.4 and b == -56e-2)
+assert(d == 'second line' and e == 'third line')
+assert(h == [[
 
 and the rest of the file
 ]])
@@ -225,45 +228,47 @@ do
   local fr = assert(io.open(file, "r"))
   assert(f:setvbuf("full", 2000))
   f:write("x")
-  assert(fr:read("*all") == "")  -- full buffer; output not written yet
+  assert(fr:read("*all") == "") -- full buffer; output not written yet
   f:close()
   fr:seek("set")
-  assert(fr:read("*all") == "x")   -- `close' flushes it
+  assert(fr:read("*all") == "x") -- `close' flushes it
   f = assert(io.open(file), "w")
   assert(f:setvbuf("no"))
   f:write("x")
   fr:seek("set")
-  assert(fr:read("*all") == "x")  -- no buffer; output is ready
+  assert(fr:read("*all") == "x") -- no buffer; output is ready
   f:close()
   f = assert(io.open(file, "a"))
   assert(f:setvbuf("line"))
   f:write("x")
   fr:seek("set", 1)
-  assert(fr:read("*all") == "")   -- line buffer; no output without `\n'
+  assert(fr:read("*all") == "") -- line buffer; no output without `\n'
   f:write("a\n")
   fr:seek("set", 1)
-  assert(fr:read("*all") == "xa\n")  -- now we have a whole line
-  f:close(); fr:close()
+  assert(fr:read("*all") == "xa\n") -- now we have a whole line
+  f:close();
+  fr:close()
 end
 
 
 -- testing large files (> BUFSIZ)
 io.output(file)
-for i=1,5001 do io.write('0123456789123') end
+for i = 1, 5001 do io.write('0123456789123') end
 io.write('\n12346')
 io.close()
 io.input(file)
 local x = io.read('*a')
 io.input():seek('set', 0)
-local y = io.read(30001)..io.read(1005)..io.read(0)..io.read(1)..io.read(100003)
-assert(x == y and string.len(x) == 5001*13 + 6)
+local y = io.read(30001) .. io.read(1005) .. io.read(0) .. io.read(1) .. io.read(100003)
+assert(x == y and string.len(x) == 5001 * 13 + 6)
 io.input():seek('set', 0)
-y = io.read()  -- huge line
-assert(x == y..'\n'..io.read())
+y = io.read() -- huge line
+assert(x == y .. '\n' .. io.read())
 assert(io.read() == nil)
 io.close(io.input())
 assert(os.remove(file))
-x = nil; y = nil
+x = nil;
+y = nil
 
 x, y = pcall(io.popen, "ls")
 if x then
@@ -273,7 +278,7 @@ else
   (Message or print)('\a\n >>> popen not available<<<\n\a')
 end
 
-print'+'
+print '+'
 
 local t = os.time()
 T = os.date("*t", t)
@@ -294,22 +299,22 @@ do
   assert(type(T.isdst) == 'boolean')
   T.isdst = nil
   local t1 = os.time(T)
-  assert(t == t1)   -- if isdst is absent uses correct default
-end   
+  assert(t == t1) -- if isdst is absent uses correct default
+end
 
 t = os.time(T)
-T.year = T.year-1;
+T.year = T.year - 1;
 local t1 = os.time(T)
 -- allow for leap years
-assert(math.abs(os.difftime(t,t1)/(24*3600) - 365) < 2)
+assert(math.abs(os.difftime(t, t1) / (24 * 3600) - 365) < 2)
 
 t = os.time()
 t1 = os.time(os.date("*t"))
-assert(os.difftime(t1,t) <= 2)
+assert(os.difftime(t1, t) <= 2)
 
-local t1 = os.time{year=2000, month=10, day=1, hour=23, min=12, sec=17}
-local t2 = os.time{year=2000, month=10, day=1, hour=23, min=10, sec=19}
-assert(os.difftime(t1,t2) == 60*2-2)
+local t1 = os.time { year = 2000, month = 10, day = 1, hour = 23, min = 12, sec = 17 }
+local t2 = os.time { year = 2000, month = 10, day = 1, hour = 23, min = 10, sec = 19 }
+assert(os.difftime(t1, t2) == 60 * 2 - 2)
 
 io.output(io.stdout)
 local d = os.date('%d')
