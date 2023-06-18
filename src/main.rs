884162cf61ec345843ece5cdae27b43fb595c58b
MarkosTh09 <markos.theocharis2009@gmail.com>
2023-06-18 14:36:03 +0400

    test: fix tests

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,36 @@ mod test {
     use probe::{lexer::scan_code, parser::parse_tokens};
     use tests_proc_macro::gen_tests;
 
+    macro_rules! settings {
+        ($test:literal, $path:expr) => {{
+            let path = &$path;
+            let dir = {
+                let mut path = path.ancestors().nth(1).unwrap().to_path_buf();
+                path.push(concat!("snapshots/", $test));
+                PathBuf::from("../").join(path)
+            };
+            let name = path.file_name().unwrap().to_string_lossy().to_string();
+
+            let mut settings = insta::Settings::new();
+            settings.set_snapshot_path(dir);
+            settings.set_input_file(path.to_string_lossy().to_string());
+            settings.set_snapshot_suffix(name);
+            settings.set_prepend_module_to_snapshot(false);
+            settings
+        }};
+    }
+
     fn scan(path: PathBuf) -> Result<(), String> {
-        let code = std::fs::read_to_string(path).unwrap();
+        let code = std::fs::read_to_string(&path).unwrap();
         let scanned = scan_code(code)?;
-        insta::assert_debug_snapshot!(scanned);
+        let settings = settings!("lexer", path);
+
+        settings.bind(|| {
+            insta::assert_debug_snapshot!(scanned);
+        });
 
         Ok(())
     }
 
-    gen_tests!("lua5.1-tests", scan);
-    // gen_tests!("lua5.2-tests");
-    // gen_tests!("lua5.3-tests");
-    // gen_tests!("lua5.4-tests");
+    gen_tests!("test-data/lua5.1-tests", scan);
 }
